ed7aaf56d73309864a05ceaf6c3adaa8283da878
ted <tfarley1990@gmail.com>
2021-07-21 15:19:39 -0400

    simple union of metadata

--- dbt/models/staging/metadata/agg_metadata.sql
+++ dbt/models/staging/metadata/agg_metadata.sql
@@ -1,27 +1,9 @@
+select
+    *
+from `pluto-panel.metadata.metadata_2002_2003`
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
+union all 
 
-    Try changing "table" to "view" below
-*/
-
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+select
+    *
+from `pluto-panel.metadata.metadata_2004_2020`
