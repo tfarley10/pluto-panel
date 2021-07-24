83fb4bc86ec8a2dcf3d54a43756cd781f36878f0
ted <tfarley1990@gmail.com>
2021-07-24 07:24:10 -0400

    Create get_pluto_tables.sql

---
+++ dbt/macros/get_pluto_tables.sql
@@ -0,0 +1,21 @@
+{% macro get_pluto_tables() %}
+
+{% set pluto_tables_query %}
+select distinct
+table_name
+from mydataset.INFORMATION_SCHEMA.TABLES
+order by 1
+{% endset %}
+
+{% set results = run_query(pluto_tables_query) %}
+
+{% if execute %}
+{# Return the first column #}
+{% set results_list = results.columns[0].values() %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+{{ return(results_list) }}
+
+{% endmacro %}
