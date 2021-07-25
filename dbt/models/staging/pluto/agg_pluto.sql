fb3263b28e9b4b2267abe4864a8dce903e9e240d
ted <tfarley1990@gmail.com>
2021-07-25 11:58:46 -0400

    add columns

--- dbt/models/staging/pluto/agg_pluto.sql
+++ dbt/models/staging/pluto/agg_pluto.sql
@@ -1,6 +1,6 @@
 {% set pluto_tables = get_pluto_tables() %}
 {% set max_far_col = ["residfar", "maxallwfar"] %}
-{% set lot_type_col = ["landuse", "landuse2"] %}
+{% set built_far_col = ["far", "builtfar"] %}
 
 {% for tb in pluto_tables %}
 select 
@@ -17,11 +17,20 @@ select
     lpad(cast(block as string), 5, '0') as block,
     lpad(cast(lot as string), 4, '0') as lot,
     {{across(dbtplyr.one_of(max_far_col, source('pluto', tb )), "{{var}}")}} as max_resid_allw_far,
+    {{across(dbtplyr.one_of(built_far_col, source('pluto', tb )), "{{var}}")}} as built_far,
     {{across(dbtplyr.starts_with('landuse', source('pluto', tb )), "{{var}}")}} as land_use_category,
     zonedist1 as primary_zoning_district,
     ownertype as owner_type,
     ownername as owner_name,
-    numbldgs as num_buildings,
+    resarea as residentail_floor_area,
+    comarea as commercial_floor_area,
+    numbldgs as count_buildings,
+    numfloors as total_floors,
+    unitstotal as total_units,
+    unitsres as total_residential_units,
+    assessland as assessed_land_value,
+    yearbuilt as year_built,
+    address as lot_address,
     cast(histdist as string) as historic_district,
     safe.st_geogfromtext(wkt_geom) as geom
 from {{source('pluto', tb )}}
