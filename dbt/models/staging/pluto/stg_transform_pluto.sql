24c8e53a11384b2ef3e19ce0de27f661cf513306
ted <tfarley1990@gmail.com>
2021-07-25 13:40:14 -0400

    add stg_* prefix

--- dbt/models/staging/pluto/stg_transform_pluto.sql
+++ dbt/models/staging/pluto/stg_transform_pluto.sql
@@ -1,5 +1,8 @@
+{{config(
+    materialized = "table"
+)}}
 select 
     *,
     md5(year || borough_code || block || lot) as pluto_year_id,
     borough_code || block || lot as bbl
-from {{ref('agg_pluto')}}
+from {{ref('stg_agg_pluto')}}
