a1750a7317a4b932f6397bc252d3a831ce49a04f
ted <tfarley1990@gmail.com>
2021-07-28 16:53:37 -0400

    :finnadie:

--- pluto-bq-ingest/pluto_parquet.py
+++ pluto-bq-ingest/pluto_parquet.py
@@ -3,10 +3,37 @@ import geopandas as gpd
 from janitor import clean_names
 import fiona
 from functools import partial
+import os
+
+import logging
+from logging.config import fileConfig
+
+loginipath='logging_config.ini'
+fileConfig(loginipath, defaults={'logfilename': 'mylog.log'})
+logger = logging.getLogger('sLogger')
+
+def list_shp(dir:str):
+
+    """
+
+    :param dir: directory to walk
+    :param filetypes: list of filetypes that should be returned (eg. [pdf,htm])
+    :return: list of paths to files specified by filetypes
+    """
+    paths=[]
+    get_ext = lambda x: x.rsplit('.', 1)[1].lower()
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if get_ext(file) == 'shp':
+                paths.append(os.path.join(root, file))
+    return (paths)
+
+
 
 
 def clipped(path: str):
-    return ('unclipped' not in path) and ('mappluto' in path)
+    lower_path=path.lower()
+    return ('unclipped' not in lower_path) and ('mappluto' in lower_path)
 
 
 def filter_clipped(path_list: list):
@@ -43,13 +70,15 @@ def read_chunk(path, chunk:slice):
     :return: geodataframe
     """
     name = file_name(path)
-    print(f'reading from {chunk.start} to {chunk.stop} rows from {name}')
+    msg=f'reading from {chunk.start} to {chunk.stop} rows from {name}'
+    logger.info(msg)
     return gpd.read_file(path, rows=chunk)
 
 
 def transform_gdf(gdf: gpd.GeoDataFrame):
 
-    print("transforming dataframe")
+    msg="transforming dataframe"
+    logger.info(msg)
     transformed = gdf.copy(deep=False)
     transformed = transformed.to_crs(epsg = 4326)
     transformed['geometry'] = transformed["geometry"].fillna()
@@ -65,12 +94,11 @@ def read_transform(path: str, chunk: slice):
 
 def agg_from_path(pth):
     chunks = make_chunks(path=pth)
-#     read_t = partial(read_transform, path=pth)
     g_list=[]
     for c in chunks:
         g_list.append(read_transform(pth, c))
 
-    return 
+    return pd.concat(g_list)
 
 
 
