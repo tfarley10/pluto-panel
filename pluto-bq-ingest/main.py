68f0d81defca2a1f6d3e8c9dc86105be45e7ebca
ted <tfarley1990@gmail.com>
2021-07-30 14:54:50 -0400

    make it work!

--- pluto-bq-ingest/main.py
+++ pluto-bq-ingest/main.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from utils import list_all_files
-from google.cloud import storage
-# import etc.secret
-from zips import unzip_to_temp
-from pluto_year import PlutoYear
-from shutil import rmtree
 import os
+import pluto_parquet as pp
+from zips import unzip_to_temp
+from shutil import rmtree
+from functools import partial
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'etc/secret.json'
 
 client = storage.Client()
 bucket = client.bucket('raw-pluto')
@@ -28,6 +28,17 @@ for index, row in zip_links.iterrows():
     rmtree(dir)
     blob.upload_from_filename('./tmp_data.csv')
     os.remove('tmp_data.csv')
+    
+    
+def main():
+    zips=pd.read_csv("etc/zip_links.csv")
+    link_2002=zips.iloc[0,0]
+    
+    dirr=unzip_to_temp(link_2002)
+    
+    paths=pp.list_shp(dirr)
+    root_paths=['..' + p for p in paths]
+    agg_year = partial(pp.agg_from_path, year=str(2002))
 
 
 if __name__ == '__main__':
