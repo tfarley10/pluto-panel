6d7c6cd6ecbf0f94026729ad34bb1f4a63f5793a
ted <tfarley1990@gmail.com>
2021-07-31 16:00:51 -0400

    comment, remove unused imports

--- pluto-bq-ingest/zips.py
+++ pluto-bq-ingest/zips.py
@@ -2,14 +2,11 @@ from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
 from tempfile import mkdtemp
-from utils import list_file_types
+import utils
 import os
 import logging
-from logging.config import fileConfig
 
-loginipath='logs/logging_config.ini'
-log_file='extract_load.log'
-log_path=os.path.join('logs', log_file)
+
 logger = logging.getLogger('sLogger')
 
 
@@ -26,23 +23,39 @@ def temp_sub(file):
 
 def dir_dic(zip_list):
 
-    """Creates tmp directories for zipfiles, creates dictionary for zipfile -> directory"""
+    """
+    Creates tmp directories for zipfiles, creates dictionary for zipfile -> directory
+    """
     zip_dir = [temp_sub(x) for x in zip_list]
     zip_dict = dict(zip(zip_list, zip_dir))
     return(zip_dict)
 
-def unzip_child(zip_list, extract = True):
+def unzip_child(zip_list):
+    """
+    only called when there are nested zipfiles
+    """
     
     zip_dict = dir_dic(zip_list)
 
     for file, dir in zip_dict.items():
         with ZipFile(file) as zfile:
-            if not extract:
-                return(zfile.namelist())
-            else:
                 zfile.extractall(dir)
 
-def unzip_to_temp(zipurl, extract = True):
+
+def filter_zips(dir:str) -> list:
+    """
+    from a directory, return all zipfiles
+    """
+
+    paths=utils.list_files(dir)
+    is_zip=lambda x: x.rsplit('.', 1)[1].lower() == 'zip'
+
+    filtered_paths=filter(is_zip, paths)
+
+    return list(filtered_paths)
+
+
+def unzip_to_temp(zipurl) -> str:
 
     """
 
@@ -56,23 +69,18 @@ def unzip_to_temp(zipurl, extract = True):
         msg=f"Downloading ZIPFile {zipurl}"
         logger.info(msg)
         with ZipFile(BytesIO(zipresp.read())) as zfile:
-            if not extract:
-                files = zfile.namelist()
-                return files
-            else:
-
-                logger.info(f'Unzipping {zipurl}')
-                zfile.extractall(dir)
+            logger.info(f'Unzipping {zipurl}')
+            zfile.extractall(dir)
 
     # does the shapefile contain more zipfiles?
-    child_zips = list_file_types(dir, ['zip'])
+    child_zips = filter_zips(dir)
     len_child=len(child_zips)
     if len_child > 0:
         msg2=f'{zipurl} has {len_child} nested zipfiles, unzipping'
         logger.info(msg2)
         unzip_child(child_zips)
 
-    return(dir)
+    return dir
     
 
 
