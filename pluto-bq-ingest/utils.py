8d895a07fc74e13fd9738cb6e4396beb976790a4
ted <tfarley1990@gmail.com>
2021-08-01 16:04:13 -0400

    Update utils.py

--- pluto-bq-ingest/utils.py
+++ pluto-bq-ingest/utils.py
@@ -1,8 +1,11 @@
 import os
 import psutil
+import httplib2 
+from bs4 import BeautifulSoup, SoupStrainer
+from typing import List
 
 
-def list_files(dir:str) -> list:
+def list_files(dir:str) -> List[str]:
     """
     returns all files given a root directory
     """
@@ -12,6 +15,16 @@ def list_files(dir:str) -> list:
         for file in files:
                 paths.append(os.path.join(root, file))
     return paths
+
+
+def get_links(url_path:str) -> List[str]:
+    
+    h = httplib2.Http()
+    status, response = h.request(url_path)
+    a_tags = BeautifulSoup(response, 'html.parser', parse_only=SoupStrainer('a'))
+    links = [link['href'] for link in a_tags if link.has_attr('href')]
+    
+    return links
 
 
 def cpu_use():
