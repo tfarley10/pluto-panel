1557f167a14ee6c093a72f776c5f029ac234abc5
ted <tfarley1990@gmail.com>
2021-08-03 18:23:56 -0400

    saves wget to csv file

--- pluto-bq-ingest/scripts/pluto_zips.sh
+++ pluto-bq-ingest/scripts/pluto_zips.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-wget -qO- https://www1.nyc.gov/site/planning/data-maps/open-data/bytes-archive.page?sorts[year]=0/ |
+
+# loads pluto links to foo.csv
+
+links=$(wget -qO- https://www1.nyc.gov/site/planning/data-maps/open-data/bytes-archive.page?sorts[year]=0/ |
 grep -Eoi '<a [^>]+>' | 
 grep -Eo 'href="[^\"]+"' | 
 grep -Eo '[^href=\"].+mappluto.+\.zip' |
-grep -Ev 'fgdb' 
+grep -Ev 'fgdb');
+
+echo "link, date" >> foo.csv;
+for i in $links; 
+do ts=$(gdate -u +'%Y-%m-%dT%H:%M:%S.%3NZ'); 
+echo "${i}, $ts" >> foo.csv;
+done
