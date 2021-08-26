cf039e30a20256bf51d0828786cc10f33523a870
ted <tfarley1990@gmail.com>
2021-08-26 13:42:22 -0400

    create max_resid_allw_far_diff

--- dbt/models/staging/pluto/stg_transform_pluto.sql
+++ dbt/models/staging/pluto/stg_transform_pluto.sql
@@ -1,8 +1,17 @@
 {{config(
     materialized = "table"
 )}}
+
+with prep as (
 select 
     *,
     md5(year || borough_code || block || lot) as pluto_year_id,
     borough_code || block || lot as bbl
 from {{ref('stg_agg_pluto')}}
+
+)
+
+select
+    *,
+    max_resid_allw_far - lag(max_resid_allw_far) over(partition by bbl order by year asc) as max_resid_allw_far_diff
+from prep
