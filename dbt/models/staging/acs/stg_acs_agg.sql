cdfbb3c5b6b6a046eca9bbfabc5c19b81a15b5d6
ted <tfarley1990@gmail.com>
2021-08-28 16:22:18 -0400

    aggregate acs as view

---
+++ dbt/models/staging/acs/stg_acs_agg.sql
@@ -0,0 +1,19 @@
+{% set acs_years = ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'] %}
+
+
+{% for yr in acs_years %}
+select 
+    geo_id,
+    family_households,
+    housing_units,
+    median_rent,
+    mortgaged_housing_units,
+    gini_index,
+    no_car,
+    not_us_citizen_pop,
+    masters_degree,
+    bachelors_degree,
+    {{yr}} as year
+from `bigquery-public-data.census_bureau_acs.puma_{{yr}}_1yr`
+{% if not loop.last -%} union all {%- endif %}
+{% endfor %}
