802f7c99de97b062c6ee940e84a164d929910efb
ted <tfarley1990@gmail.com>
2021-08-28 13:25:36 -0400

    change secret location

--- pluto-bq-ingest/storage_to_bq.py
+++ pluto-bq-ingest/storage_to_bq.py
@@ -1,9 +1,10 @@
 # https://cloud.google.com/bigquery/docs/loading-data-cloud-storage-parquet
 from google.cloud import storage
 from google.cloud import bigquery
-import etc.secret
 from google.cloud.exceptions import NotFound
+import os
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'secret.json'
 storage_client = storage.Client()
 bq_client = bigquery.Client()
 
@@ -30,6 +31,8 @@ def load_from_uri(uri, table_id):
         load_job.result()  # Waits for the job to complete.
         destination_table = bq_client.get_table(table_id)
         print(f"Loaded {destination_table.num_rows} rows to {table_id}.")
+        
+def main():
 
-for uri, table in storage_bq_map.items():
-    load_from_uri(uri = uri, table_id = table)
+    for uri, table in storage_bq_map.items():
+        load_from_uri(uri = uri, table_id = table)
