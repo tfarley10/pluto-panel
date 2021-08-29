ad51d221af2c68633965b403659ca2323941631a
ted <tfarley1990@gmail.com>
2021-08-29 09:23:19 -0400

    comments

--- dbt/models/staging/pluto/stg_agg_pluto.sql
+++ dbt/models/staging/pluto/stg_agg_pluto.sql
@@ -1,3 +1,4 @@
+
 {% set pluto_tables = get_pluto_tables() %}
 {% set max_far_col = ["residfar", "maxallwfar"] %}
 {% set built_far_col = ["far", "builtfar"] %}
@@ -6,6 +7,8 @@
 select 
     split('{{tb}}', '_')[safe_offset(1)] as year,
     borough,
+
+    {# standardizing bbl code across years #}
     case borough 
         when 'MN' then '1'
         when 'BX' then '2'
@@ -16,12 +19,13 @@ select
     end as borough_code,
     lpad(cast(block as string), 5, '0') as block,
     lpad(cast(lot as string), 4, '0') as lot,
-    {# pick first column name #}
+
+    {# workaround to fix inconsistent column names #}
     {{across(dbtplyr.one_of(max_far_col, source('raw_pluto', tb )), "{{var}}")}} as max_resid_allw_far,
     {{across(dbtplyr.one_of(built_far_col, source('raw_pluto', tb )), "{{var}}")}} as built_far,
-    {{across(dbtplyr.starts_with('landuse', source('raw_pluto', tb )), "{{var}}")}} as land_use_category,
+    {{across(dbtplyr.starts_with('landuse', source('raw_pluto', tb )), "{{var}}")}} as land_use_code,
     zonedist1 as primary_zoning_district,
-    nullif(lower(trim(ownertype)), '') as owner_type,
+    nullif(lower(trim(ownertype)), '') as owner_type_code,
     ownername as owner_name,
     resarea as residentail_floor_area,
     comarea as commercial_floor_area,
