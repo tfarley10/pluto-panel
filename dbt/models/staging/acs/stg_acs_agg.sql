999096ba144a528557bd468c19e7baf0e6144b90
ted <tfarley1990@gmail.com>
2021-09-25 12:19:47 -0400

    adds no_cars, pop_25_64

--- dbt/models/staging/acs/stg_acs_agg.sql
+++ dbt/models/staging/acs/stg_acs_agg.sql
@@ -1,17 +1,20 @@
-{% set acs_years = ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'] %}
+{% set acs_years = ['2007','2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'] %}
 
 
 {% for yr in acs_years %}
 select 
     geo_id,
     total_pop,
+    pop_25_64,
     family_households,
+    households,
     median_income,
     housing_units,
     median_rent,
     mortgaged_housing_units,
     gini_index,
-    no_car,
+    {# number of households w/o a car #}
+    no_cars,
     not_us_citizen_pop,
     masters_degree,
     bachelors_degree,
