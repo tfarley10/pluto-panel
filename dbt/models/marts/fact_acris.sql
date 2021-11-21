65a7215294a5ae3280db6d0afa6091d5862d7c34
ted <tfarley1990@gmail.com>
2021-11-21 15:20:01 -0500

    acris fact table

---
+++ dbt/models/marts/fact_acris.sql
@@ -0,0 +1,17 @@
+{{
+    config(
+        tags = ["acris"]
+    )
+}}
+
+with a as (
+    select 
+        master.* except(party1_type, party2_type, party3_type),
+        legals.property_type,
+        legals.bbl
+    from {{ref('stg_acris_master')}}  as master
+    left join {{ref('stg_acris_legals')}} as legals using (document_id)
+)
+
+select * from a
+
