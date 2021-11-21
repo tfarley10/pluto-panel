a3072fdf31b92624a4794d4664bda91954ba5272
ted <tfarley1990@gmail.com>
2021-11-21 15:20:44 -0500

    pd

--- dbt/target/compiled/pluto/models/staging/real_estate/stg_acris_master.sql
+++ dbt/target/compiled/pluto/models/staging/real_estate/stg_acris_master.sql
@@ -12,13 +12,14 @@ select
     crfn,
     date(split(recorded_datetime, 'T')[offset(0)]) as recorded_date,
     modified_date,
-    percent_trans,
+    round(cast(percent_trans as numeric), 2) as percent_trans,
     good_through_date,
     doc_type_description,
     class_code_description,
+    cast(document_amt as numeric) as amount,
     party1_type,
     party2_type,
     party3_type
 
 from a 
-left join `pluto-panel`.`dbt_ted`.`acris_document_codes` using (doc_type)
+left join `pluto-panel`.`dbt_ted_seed_data`.`acris_document_codes` using (doc_type)
