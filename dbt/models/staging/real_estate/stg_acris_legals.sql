a49f40352ccdf418b28a306888e6461132d52e9e
ted <tfarley1990@gmail.com>
2021-11-21 15:20:21 -0500

    Create stg_acris_legals.sql

---
+++ dbt/models/staging/real_estate/stg_acris_legals.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        tags = ["acris"]
+    )
+}}
+
+with a as (
+    select 
+        document_id,
+        borough as borough_code,
+        lpad(block, 4, '0') as block,
+        lpad(lot, 5, '0') as lot,
+        if(easement = 'Y', true, false) as is_easement,
+        if(partial_lot = 'P', true, false) as is_partial,
+        if(air_rights = 'Y', true, false) as is_air_rights,
+        date(split(good_through_date, 'T')[offset(0)]) as good_through_date,
+        property_type as property_type_code,
+        street_number,
+        street_name,
+        unit
+    from {{source('real_estate','raw_acris_legals')}}
+)
+
+select 
+    a.*,
+    property_type,
+    borough_code || block || lot as bbl,
+    farm_fingerprint(borough_code || block || lot) as bbl_hash
+from a 
+left join {{ref('property_codes')}} using (property_type_code)
